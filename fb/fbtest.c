4006943eaaa9f7c12df5e691a5ec1734a411c589
rst- <jprosti@iki.fi>
2016-03-04 09:49:57 +0000

    More info output.

--- fb/fbtest.c
+++ fb/fbtest.c
@@ -27,6 +27,7 @@ int main(int argc, char* argv[])
 {
     int fbfd = 0; // framebuffer filedescriptor
     struct fb_var_screeninfo var_info;
+    struct fb_fix_screeninfo fix_info;
 
     // Open the framebuffer device file for reading and writing
     fbfd = open("/dev/fb0", O_RDWR);
@@ -36,17 +37,33 @@ int main(int argc, char* argv[])
     }
     printf("The framebuffer device opened.\n");
 
+    // Get fixed screen information
+    if (ioctl(fbfd, FBIOGET_FSCREENINFO, &fix_info)) {
+        printf("Error reading fixed screen info.\n");
+    }
+    printf("Fixed info:\n");
+    printf(" id = %s\n type=%d\n accel=%d\n",
+                fix_info.id,
+                fix_info.type,
+                fix_info.accel
+	);
+    printf(" smem_len=%d\n line_length=%d\n capabs=%d\n",
+		fix_info.smem_len,
+                fix_info.line_length,
+                fix_info.capabilities
+	);
+
     // Get variable screen information
     if (ioctl(fbfd, FBIOGET_VSCREENINFO, &var_info)) {
         printf("Error reading variable screen info.\n");
     }
-    printf("Display info %dx%d, %d bpp\n", 
-                 var_info.xres, var_info.yres, 
+    printf("Variable info:\n %dx%d, %d bpp\n",
+                 var_info.xres, var_info.yres,
                  var_info.bits_per_pixel );
 
-    // close fb file    
+    // close fb file
     close(fbfd);
-    
+
     return 0;
 
 }
